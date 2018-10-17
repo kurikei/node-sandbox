bda75012116f3e04aa1eda57f2b45f93795fc58e
kurikei <kurikei@gmail.com>
2018-10-17 20:20:34 +0900

    firestore からデータを取得する

--- firebase-admin/firestore/src/index.ts
+++ firebase-admin/firestore/src/index.ts
@@ -1,4 +1,20 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.PROJECT_ID);
+import * as admin from 'firebase-admin';
+const serviceAccount = require(process.env.CREDENTIAL_FILE_PATH);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+db.collection('messages').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
